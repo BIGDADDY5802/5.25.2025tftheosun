ae70282d910d1de8244f8d5450d955b6e273d5ef
Jerome Hunter Sr. <Firstofmyname5802@gmail.com>
2025-05-25 17:15:18 -0500

    Update 5-nat.tf

--- 5-nat.tf
+++ 5-nat.tf
@@ -1,8 +1,34 @@
-#4-router.tf
+#5-nat.tf
 
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_router
-resource "google_compute_router" "router" {
-  name    = "router"
-  region  = "us-central1"
-  network = google_compute_network.cwglobal.id
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_router_nat
+resource "google_compute_router_nat" "nat" {
+  name   = "nat"
+  router = google_compute_router.router.name
+  region = "us-central1"
+
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  nat_ip_allocate_option             = "AUTO_ONLY"
+
+  subnetwork {
+    name                    = google_compute_subnetwork.private.id
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+
+  
+}
+
+resource "google_compute_router_nat" "nat2" {
+  name   = "nat2"
+  router = google_compute_router.router2.name
+  region = "southamerica-east1"
+
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  nat_ip_allocate_option             = "AUTO_ONLY"
+
+  subnetwork {
+    name                    = google_compute_subnetwork.private2.id
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+
+  
 }
