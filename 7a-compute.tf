eb89c8a4832965761ed0ae5cc6b2cdc8287b1164
Jerome Hunter Sr. <Firstofmyname5802@gmail.com>
2025-05-25 16:21:30 -0500

    Create 7a-compute.tf

---
+++ 7a-compute.tf
@@ -0,0 +1,72 @@
+#7a-compute.tf
+
+resource "google_compute_instance" "iowavm" {
+  name         = "iowavm"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.private.name
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata_startup_script = file("./startup.sh")
+
+}
+
+
+
+resource "google_compute_instance" "saopaolovm" {
+  name         = "saopaolovm"
+  machine_type = "e2-medium"
+  zone         = "southamerica-east1-a"
+
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.private2.name
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata_startup_script = file("./startup.sh")
+
+}
+
+resource "google_compute_instance" "tokyovm" {
+  name         = "tokyovm"
+  machine_type = "e2-medium"
+  zone         = "asia-northeast1-a"
+
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.private3.name
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata_startup_script = file("./startup.sh")
+
+}
