c8e2ee9ec6fdbb949f5ee41e390bc7ea32d71724
Jerome Hunter Sr. <Firstofmyname5802@gmail.com>
2025-05-25 22:07:25 -0500

    Update 5-nat.tf

--- 5-nat.tf
+++ 5-nat.tf
@@ -16,19 +16,3 @@ resource "google_compute_router_nat" "nat" {
 
   
 }
-
-resource "google_compute_router_nat" "nat2" {
-  name   = "nat2"
-  router = google_compute_router.router2.name
-  region = "southamerica-east1"
-
-  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
-  nat_ip_allocate_option             = "AUTO_ONLY"
-
-  subnetwork {
-    name                    = google_compute_subnetwork.private2.id
-    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
-  }
-
-  
-}
